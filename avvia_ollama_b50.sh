e63f63b74bbad65025150500ed1bb098ebc5b519
Andrea <chiappi.andrea@gmail.com>
2026-01-21 17:32:56 +0100

    first commit

---
+++ avvia_ollama_b50.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
+
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate jarvis
+
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
+# Parametri Ollama per Intel GPU
+export OLLAMA_NUM_PARALLEL=1
+export OLLAMA_CONTEXT_LENGTH=4096
+
+# Gestione risorse
+export ONEAPI_DEVICE_SELECTOR=level_zero:0
+export OLLAMA_INTEL_GPU=1
+
+echo "----------------------------------------"
+echo " Avvio Ollama su intel Arc Pro B50      "
+echo " Ambiente: jarvis IPEX 2.8.0+xpu attivo " 
+echo "----------------------------------------"
+
+cd ~/llama-ccp
+./ollama serve
