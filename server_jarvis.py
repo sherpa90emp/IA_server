8ef84e928cb04c9a767d6800197b85e2b0d0e14b
Andrea <chiappi.andrea@gmail.com>
2026-02-14 16:42:25 +0100

    mod

--- server_jarvis.py
+++ server_jarvis.py
@@ -1,4 +1,5 @@
 import os
+import re
 import json
 import threading
 from queue import Queue, Empty
@@ -82,8 +83,6 @@ except Exception as e :
 app = FastAPI()
 
 model_lock = threading.Lock()
-
-def 
 
 def stream_generator(prompt, max_new_tokens, is_chat=False) :
     
@@ -178,18 +177,18 @@ async def chat(request: Request) :
 async def completions(request: Request) :
     data = await request.json()
     raw_prompt = data.get("prompt", "")
-    print(f"Prompt originale: {repr(raw_prompt)}")
-    print("---------------------------------------")
+    #print(f"Prompt originale: {repr(raw_prompt)}")
+    #print("---------------------------------------")
     
     #full_prompt = raw_prompt.replace(" <|fim_middle|>", "<|fim_middle|>")
     #full_prompt = full_prompt.replace("\n<|fim_middle|>", "<|fim_middle|>")
     #full_prompt = full_prompt.replace("\r\n<|fim_middle|>", "<|fim_middle|>")
     full_prompt = re.sub(r'[\s\r\n]+<\|fim_middle\|>', '<|fim_middle|>', raw_prompt)
-    print(f"Prompt modificato: {repr(full_prompt)}")
+    #print(f"Prompt modificato: {repr(full_prompt)}")
     
     return StreamingResponse(stream_generator(
         full_prompt, 
-        max_new_tokens=64,
+        max_new_tokens=16,
         is_chat=False), 
         media_type="text/event-stream")
 
