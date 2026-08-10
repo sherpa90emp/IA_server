4e22977026a6e3b6686b18c6c0b5a538a9152f05
Andrea <chiappi.andrea@gmail.com>
2026-08-10 22:13:55 +0200

    v 0.12.0 - aggiunto controllo folder/file

--- utilities/general_func.py
+++ utilities/general_func.py
@@ -20,5 +20,28 @@ def rileva_device():
 
     return model_device_name_GPU, model_device_name_CPU, target_device
 
-def compose_path(genric_dir, file):
-    return os.path.join(genric_dir, file)
+def compose_path(generic_dir, file):
+    return os.path.join(generic_dir, file)
+
+def print_all_contents(generic_dir):
+    contents = os.listdir(generic_dir)
+    for i, content in enumerate(contents):
+        to_check = check_file(generic_dir, content)
+        if to_check:
+            print(f"{i} - [FILE] {content}")
+        else:
+            print(f"{i} - [FOLDER] {content}")
+
+def check_file(base_dir, generic_dir) -> bool:
+    to_check = os.path.join(base_dir, generic_dir)
+    if os.path.isfile(to_check):
+        return True
+    else:
+        return False
+
+def check_folder(generic_dir) -> bool:
+    contents = os.listdir(generic_dir)
+    for f in contents:
+        if os.path.isdir(os.path.join(generic_dir, f)):
+            return True
+    return False
