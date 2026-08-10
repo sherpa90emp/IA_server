854c0dcb02d43e922e888d9ad94692bc7ecd71b6
Andrea <chiappi.andrea@gmail.com>
2026-08-10 22:14:27 +0200

    v 0.12.1 - implementato il controllo folder/file

--- tools/embedding.py
+++ tools/embedding.py
@@ -1,8 +1,10 @@
 import numpy
-import  tools
+import sys
+import os
+import tools
 from tools.lettura_file import get_all_files, select_file, read_file
 from utilities.color_logger import ColoreLog
-from utilities.general_func import compose_path
+from utilities.general_func import compose_path, print_all_contents, check_folder
 
 def generate_embedding(emb_name, emb_model, emb_tokenizer, chunks):
     if chunks is None:
@@ -35,16 +37,30 @@ def generate_embedding(emb_name, emb_model, emb_tokenizer, chunks):
 
 def select_file_for_emb():
     try:
-        print("\nIn quale cartella si trova il file?\n")
-        user_dir = input()
-        if user_dir:
-            file_list =  get_all_files(user_dir)
-            selected_file = select_file(file_list)
-            folder_dir = compose_path(tools.lettura_file.FILE_DIR, user_dir)
-            selected_file_path = compose_path(folder_dir, selected_file)
-            print(f"{ColoreLog.INFO}[INFO]{ColoreLog.RESET} Percorso del file selezionato: {selected_file_path}")
-            input_text = read_file(selected_file_path)
-            return input_text
+        current_dir = tools.lettura_file.FILE_DIR
+        print(f"\n{ColoreLog.INPUT}[INPUT]{ColoreLog.RESET}In quale cartella si trova il file?\n")
+        while True:
+            print_all_contents(current_dir)
+            user_dir = input()
+            if user_dir.lower() == "exit":
+                sys.exit()
+            folder_dir = compose_path(current_dir, user_dir)
+            print(f"\n{ColoreLog.DEBUG}[DEBUG]{ColoreLog.RESET} Percorso selezionato: {folder_dir}\n")
+            if not os.path.exists(folder_dir):
+                print(f"{ColoreLog.ERRORE}[ERROR]{ColoreLog.RESET} Path inesistente.")
+                continue
+            check = check_folder(folder_dir)
+            if check:
+                current_dir = folder_dir
+                print(f"\n{ColoreLog.INPUT}[INPUT]{ColoreLog.RESET}In quale cartella si trova il file?\n")
+                continue
+            else:
+                file_list =  get_all_files(user_dir)
+                selected_file = select_file(file_list)
+                selected_file_path = compose_path(folder_dir, selected_file)
+                print(f"{ColoreLog.INFO}[INFO]{ColoreLog.RESET} Percorso del file selezionato: {selected_file_path}")
+                input_text = read_file(selected_file_path)
+                return input_text           
     except Exception as e:
         print(f"{ColoreLog.ERRORE}[ERROR]{ColoreLog.RESET} Rilevato errore: {e}")
         return None
