e058d4a392777b71413f062fa01709624473d34c
Andrea <chiappi.andrea@gmail.com>
2026-09-15 07:21:09 +0200

    v 0.18.3 - debug config

--- test/test_riutilizzabile.py
+++ test/test_riutilizzabile.py
@@ -1,8 +1,9 @@
 import openvino_genai as ov_genai
 
 # Interroga l'help nativo dei binding Python
-help(ov_genai.SchedulerConfig)
+#help(ov_genai.SchedulerConfig)
 
-# Oppure crea un'istanza e stampa la sua rappresentazione stringa
-config = ov_genai.SchedulerConfig()
-print(config.to_string())
+help(ov_genai.GenerationConfig)
+
+#config = ov_genai.SchedulerConfig()
+#print(config.to_string())
